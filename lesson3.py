2b91c2112ab74254eb3aaf8496d1dd93ab48d834
liana lesnikovich <liyana.lesnikovich.run4fun@gmail.com>
2024-11-08 15:20:56 +0300

    This is a homework#3

---
+++ lesson3.py
@@ -0,0 +1,66 @@
+# Task1
+print("Task1")
+number = -1.66
+print(int(number))
+number = 2.99
+print(int(number))
+
+# Task2
+print("Task2")
+str = "www.my_site.com#about"
+new_str = str.replace("#", "/")
+print(new_str)
+
+# Task 3
+print("Task3")
+word = "stroka"
+ending = "ing"
+new_str = word + ending
+print (new_str)
+
+#Task4
+print("Task4")
+str = 'Ivanou Ivan'
+new_str = str.split(None, 2)
+print (new_str)
+print (type(new_str))
+str_changed = new_str[1] + ' ' +  new_str[0]
+print (str_changed)
+
+#Task5
+print("Task5")
+s = "  This is a line with spaces  "
+print (s)
+s = s.strip()
+print (s)
+
+#Task6
+print("Task6")
+school = {'1a': 15,
+        '1b': 11,
+        '2b': 18,
+        '6a': 20,
+        '7v': 19}
+
+#Task7
+print("Task7")
+spisok = [10, 20, 30, 40]
+print(spisok[1])
+spisok1 = ['s', 'p', 'i', 's', 'o', 'k']
+print(spisok1[1])
+
+# Task8
+print("Task8")
+txt = "employement"
+x = "employ"
+print (x in txt)
+
+# Task9
+print("Task9")
+x = "My name is Agent Smith"
+print(x[1])
+print(x[3:16:3])
+
+
+
+
